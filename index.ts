09c956816c0711c822ece948b008b39bf8e2e712
NathanAThorne <106212066+NathanAThorne@users.noreply.github.com>
2025-12-28 10:23:25 -0500

    Delete index.ts

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { getMetricsDispatcher } from "./metrics-dispatcher";
-export { getMetricsConfig } from "./metrics-config";
-export * from "./send-event";
-export { getMetricsUsageHeaders } from "./metrics-usage-headers";
